a75eceb4d0a5443aea2d0ecce7313d715e97eaa9
Kody Liou <kody.liou@gmail.com>
2024-09-11 23:14:48 +0800

    Update

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool! 123
+            Lerna is the original monorepo tool! 123 456
         </header>
     );
 }
